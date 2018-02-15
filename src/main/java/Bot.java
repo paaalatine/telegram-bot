c75be5ada4dfea910842d0dbeb278d1376e8cfd2
Sofia <kanukova.s.a@gmail.com>
2018-02-15 19:07:38 +0300

    initial

---
+++ src/main/java/Bot.java
@@ -0,0 +1,53 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi botapi = new TelegramBotsApi();
+        try {
+            botapi.registerBot(new Bot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "paaalatine_bot";
+    }
+
+    @Override
+    public void onUpdateReceived(Update e) {
+        Message msg = e.getMessage();
+        String txt = msg.getText();
+        if (txt.equals("/start")) {
+            sendMsg(msg, "добрый день!");
+        }
+        else if(txt.equals("/whoareyou")) {
+            sendMsg(msg, "я sonya!");
+        }
+    }
+
+    @Override
+    public String getBotToken() {
+        return "521024889:AAF75zgck9a6LeMM4IUeglsOcfAtlEgzb_Y";
+    }
+
+    @SuppressWarnings("deprecation") // Означает то, что в новых версиях метод уберут или заменят
+    private void sendMsg(Message msg, String text) {
+        SendMessage s = new SendMessage();
+        s.setChatId(msg.getChatId());
+        s.setText(text);
+        try {
+            sendMessage(s);
+        } catch (TelegramApiException e){
+            e.printStackTrace();
+        }
+    }
+}
